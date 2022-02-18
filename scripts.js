29ed803812e9b0de739fd8b2ab1e062af1115fd4
KadeWilliams <kadewilliams0@gmail.com>
2022-02-18 11:32:52 -0600

    functioning grid with visuals/hovering to change color

--- scripts.js
+++ scripts.js
@@ -1,21 +1,32 @@
-const container = document.querySelector('div');
-const outer = document.createElement('div')
-
+const container = document.getElementById('container');
+const button = document.querySelector('button');
 
 for (let j = 0; j < 16; j++) {
     const row = document.createElement('div');
     row.style.display = 'flex';
+    row.classList.add('row');
     for (let j = 0; j < 16; j++) {
         const col = document.createElement('div');
+        col.classList.add('col');
         col.style.display = 'flex';
         row.style.flexDirection = 'column';
-        col.style.border = '1px black solid';
+        // col.style.border = '1px black solid';
         col.style.height = '30px';
         col.style.width = '30px';
-        col.style.backgroundColor = 'white';
+        col.addEventListener('mouseover', (e) => {
+            col.style.backgroundColor = 'red';
+        })
+        // col.style.backgroundColor = 'white';
         row.appendChild(col);
+
+        button.addEventListener('click', (e) => {
+            col.style.backgroundColor = 'white';
+        })
     }
-    row.style.border = '1px black solid';
-    row.style.backgroundColor = 'grey';
+    row.childNodes[0].style.borderTop = '2px black solid';
+    row.lastChild.style.borderBottom = '2px black solid';
     container.appendChild(row);
 }
+container.childNodes[1].style.borderLeft = '2px black solid';
+container.lastChild.style.borderRight = '2px black solid';
+
