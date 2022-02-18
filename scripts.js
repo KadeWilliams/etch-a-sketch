729c70bdddb1b48c09213e9a691cde8aa4b3b0a7
KadeWilliams <kadewilliams0@gmail.com>
2022-02-17 18:11:17 -0600

    created 16x16 container with divs and centered it

---
+++ scripts.js
@@ -0,0 +1,21 @@
+const container = document.querySelector('div');
+
+
+
+for (let j = 0; j < 16; j++) {
+    const row = document.createElement('div');
+    row.style.display = 'flex';
+    for (let j = 0; j < 16; j++) {
+        const col = document.createElement('div');
+        col.style.display = 'flex';
+        row.style.flexDirection = 'column';
+        col.style.border = '1px black solid';
+        col.style.height = '30px';
+        col.style.width = '30px';
+        col.style.backgroundColor = 'blue';
+        row.appendChild(col);
+    }
+    row.style.border = '1px black solid';
+    row.style.backgroundColor = 'grey';
+    container.appendChild(row);
+}
